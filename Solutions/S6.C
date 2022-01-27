566d32102d4d939c5cd005d74034686fe1b7db0f
21bca06 <96610663+21bca06@users.noreply.github.com>
2022-01-27 09:27:56 +0530

    solution 6 uploded.....!

---
+++ Solutions/S6.C
@@ -0,0 +1,24 @@
+
+/* WAP to print following pattern
+*
+**
+***
+****
+*****
+DATE : 26/01/2022 */
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,n;
+	clrscr();
+	printf("enter a number till you are draw patttern:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+		{
+			for(j=1;j<i;j++)
+				printf("*");
+			printf("\n");
+		}
+	getch();
+}
